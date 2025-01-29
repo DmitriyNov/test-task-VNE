06c2fadf00d1299837fcafa4bdc856613e913ccb
DmitriyNov <NovokrDm@yandex.ru>
2025-01-29 15:32:06 +0300

    Initial commit

---
+++ src/models/CommentModelDataResult.ts
@@ -0,0 +1,18 @@
+export interface CommentModelDataResult {
+    total?: number;
+    data?: CommentModel[];
+}
+
+interface CommentModel {
+    id: string;
+    createDT?: string;
+    updateDT?: string;
+    user?: UserModel;
+    userGuid?: string;
+    text?: string | null;
+    isLike?: boolean | null;
+}
+
+interface UserModel {
+    username: string;
+}
